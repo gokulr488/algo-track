1135006e3a26336d0635a75de19416d23553b6a0
Gokul <gokulr488@gmail.com>
2022-11-09 16:24:36 +0530

    added common component files

---
+++ lib/common/ui_constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+Color green = const Color(0xFF0ECB81).withOpacity(0.6);
+Color red = const Color(0xFFF6465D).withOpacity(0.6);
+
+const List<Shadow> shadow = <Shadow>[
+  Shadow(offset: Offset(2, 2), blurRadius: 3)
+];
+
+const Duration kAnimDuraction = Duration(milliseconds: 350);
+const Cubic kAnimCurve = Curves.easeOut;
+
+const InputDecoration kTextFieldDecoration = InputDecoration(
+  labelStyle: TextStyle(fontSize: 14),
+  hintStyle: TextStyle(fontSize: 14),
+  contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(22)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    //borderSide: BorderSide(color: kHighlightColour),
+    borderRadius: BorderRadius.all(Radius.circular(22)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xFFd84315), width: 2),
+    borderRadius: BorderRadius.all(Radius.circular(22)),
+  ),
+);
+
+const InputDecoration kDropDownDecoration = InputDecoration(
+    labelStyle: TextStyle(
+      fontSize: 18,
+      //color: kHighlightColour,
+    ),
+    contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0x00000000)),
+      borderRadius: BorderRadius.all(Radius.circular(25)),
+    ));
